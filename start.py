6e5c89ea964cf3efa817bbf769e5ca5699b63e0b
yb17821 <37092406+yb17821@users.noreply.github.com>
2018-05-03 22:55:17 +0800

    整理不了分测试用的代码

--- start.py
+++ start.py
@@ -18,9 +18,9 @@ def w():
 
 w = threading.Thread(target=w, args=[])
 w.start()
-
 browser = Chrome('D:\webdriver\chromedriver.exe')
 browser.get('http://jwstu.cmjnu.com.cn:8080/')
+
 while True:
     go = input('go?:')
     if go == '1':
@@ -46,13 +46,9 @@ while True:
             ans_lt = ans_dic.get(queid)
 
             if not ans_lt:
+			
                 print('还没做过这道题，请先提交。')
-                # answers_areas = q.find_elements_by_xpath('.//div[@class="answerArea"]')
-                # print('answers_areas',answers_areas)
-                # for answers_area in answers_areas:
-                #     lis = answers_area.find_elements_by_xpath('.//li')
-                #     print('lis',lis)
-                #     lis[0].click()
+                
             else:
                 for i in range(len(ans_lt)):
                     if ans_lt[i] == True:
@@ -60,17 +56,17 @@ while True:
 
 
         json.dump(ans_dic, open('daan2.txt', 'w', encoding='utf-8'), ensure_ascii=False)
+		
     elif go == '2':
 
         browser.switch_to_window(browser.window_handles[-1])
         page = browser.page_source
         html_tree = etree.HTML(page)
-
         ans_dic = json.load(open('daan2.txt', 'r', encoding='utf-8'))
-
         q_list = html_tree.xpath('//div[starts-with(@class,"rep_loop")]')
 
         for q in q_list:
+		
             # 获取q_id
             queid = str(q.xpath('.//a[@queid]/@queid')[0])
 
@@ -79,7 +75,9 @@ while True:
 
             # 查找问题区域
             sq_area = q.xpath('.//div[starts-with(@class,"exercise")]')
+			
             for sq in sq_area:
+			
                 answer_num = len(sq.xpath('.//li'))
                 div = sq.xpath('.//div[@class="mb20 clearfix answer_area"]')[0]
 
@@ -88,20 +86,27 @@ while True:
                                                                                                             '').replace(
                     ' ', '')
                 sansid = str(ord(answer) - 64)
+				
                 for i in range(1, answer_num + 1):
 
                     if str(i) == sansid:
+					
                         ans_lt.append(True)
+						
                     else:
+					
                         ans_lt.append(False)
+						
             ans_dic[queid] = ans_lt
 
         for k, v in ans_dic.items():
+		
             print(k, v)
 
         json.dump(ans_dic, open('daan2.txt', 'w', encoding='utf-8'), ensure_ascii=False)
 
     elif go == 'n':
+	
         browser.quit()
         break
 
